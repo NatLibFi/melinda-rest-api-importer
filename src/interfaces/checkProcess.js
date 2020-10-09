5ccb8769b02f3c319520feca92cf586e574c26c2
natlibfi-arlehiko <natlibfi-arlehiko@users.noreply.github.com>
2020-10-09 12:43:05 +0300

    Add initial implementation (#10)
    Co-authored-by: Joni Ollila <53558796+natlibfi-jonollil@users.noreply.github.com>

---
+++ src/interfaces/checkProcess.js
@@ -0,0 +1,135 @@
+import {createLogger} from '@natlibfi/melinda-backend-commons';
+import {Error as ApiError} from '@natlibfi/melinda-commons';
+import {PRIO_QUEUE_ITEM_STATE, QUEUE_ITEM_STATE, OPERATIONS} from '@natlibfi/melinda-rest-api-commons';
+import httpStatus from 'http-status';
+import {promisify} from 'util';
+import processOperatorFactory from './processPoll';
+import {logError} from '@natlibfi/melinda-rest-api-commons/dist/utils';
+
+export default function ({amqpOperator, mongoOperator, recordLoadApiKey, recordLoadUrl, pollWaitTime}) {
+  const logger = createLogger();
+  const setTimeoutPromise = promisify(setTimeout);
+  const OPERATION_TYPES = [
+    OPERATIONS.CREATE,
+    OPERATIONS.UPDATE
+  ];
+  const processOperator = processOperatorFactory({recordLoadApiKey, recordLoadUrl});
+
+  return {intiCheck};
+
+  async function intiCheck(queue, wait = false) {
+    if (wait) {
+      await setTimeoutPromise(pollWaitTime);
+      return checkProcessQueue(queue);
+    }
+
+    await checkProcessQueue(queue);
+
+    logger.log('debug', 'Process queue done checking mongo');
+
+    const queueItem = await mongoOperator.getOne({operation: queue, queueItemState: QUEUE_ITEM_STATE.IN_PROCESS});
+    if (queueItem) {
+      return checkMongoInProcess(queueItem);
+    }
+  }
+
+  async function checkProcessQueue(queue) {
+    logger.log('verbose', `Checking process queue: PROCESS.${queue}`);
+    const processMessage = await amqpOperator.checkQueue(`PROCESS.${queue}`, 'raw');
+    try {
+      if (processMessage) {
+        const {results, processParams} = await handleProcessMessage(processMessage, queue);
+        const messagesHandled = await handleMessages({results, processParams, queue});
+        if (messagesHandled) {
+          await amqpOperator.ackMessages([processMessage]);
+          logger.log('verbose', 'Requesting file cleaning');
+          await processOperator.requestFileClear(processParams.data);
+
+          return;
+        }
+      }
+      await amqpOperator.nackMessages([processMessage]);
+    } catch (error) {
+      if (error instanceof ApiError) {
+        if (OPERATION_TYPES.includes(queue)) {
+          await sendErrorResponses(error, queue);
+          await amqpOperator.ackMessages([processMessage]);
+          return checkProcessQueue(queue);
+        }
+
+        await amqpOperator.ackMessages([processMessage]);
+        return checkProcessQueue(queue);
+      }
+    }
+  }
+
+  async function checkMongoInProcess(queueItem) {
+    const messagesAmount = await amqpOperator.checkQueue(`PROCESS.${queueItem.correlationId}`, 'messages');
+    if (messagesAmount) {
+      return checkProcessQueue(`${queueItem.correlationId}`);
+    }
+  }
+
+  async function handleProcessMessage(processMessage, queue) {
+    try {
+      const processParams = JSON.parse(processMessage.content.toString());
+      return {results: await processOperator.poll(processParams.data), processParams};
+    } catch (error) {
+      logError(error);
+      if (error instanceof ApiError) {
+        if (error.status === httpStatus.LOCKED) {
+          await amqpOperator.nackMessages([processMessage]);
+
+          return intiCheck(queue, true);
+        }
+
+        throw error;
+      }
+      // Unexpected
+      throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  async function handleMessages({results, processParams, queue}) {
+    const {headers, messages} = await amqpOperator.checkQueue(queue, processParams.correlationId);
+    if (messages) {
+      const status = headers.operation === OPERATIONS.CREATE ? 'CREATED' : 'UPDATED';
+      logger.log('verbose', 'Handling process messages based on results got from process polling');
+      // Handle separation of all ready done records
+      const ack = messages.slice(0, results.ackOnlyLength);
+      const nack = messages.slice(results.ackOnlyLength);
+      await amqpOperator.nackMessages(nack);
+      await setTimeoutPromise(100); // (S)Nack time!
+
+      if (OPERATION_TYPES.includes(queue)) {
+        // Handle separation of all ready done records
+        await ack.forEach(message => {
+          mongoOperator.setState({correlationId: message.properties.correlationId, state: PRIO_QUEUE_ITEM_STATE.DONE});
+        });
+        await amqpOperator.ackNReplyMessages({status, messages: ack, payloads: results.payloads});
+        return true;
+      }
+
+      // Ids to mongo
+      await mongoOperator.pushIds({correlationId: queue, ids: results.payloads});
+      await amqpOperator.ackMessages(ack);
+
+      return true;
+    }
+
+    return false;
+  }
+
+  async function sendErrorResponses(error, queue) {
+    logger.log('debug', 'Sending error responses');
+    const {messages} = await amqpOperator.checkQueue(queue, 'basic', false);
+    const {status} = error;
+    const payloads = error.payload ? new Array(messages.lenght).fill(error.payload) : [];
+    messages.forEach(message => {
+      mongoOperator.setState({correlationId: message.properties.correlationId, state: QUEUE_ITEM_STATE.ERROR});
+    });
+    logger.log('silly', `Status: ${status}\nMessages: ${messages}\nPayloads:${payloads}`);
+    // Send response back if PRIO
+    await amqpOperator.ackNReplyMessages({status, messages, payloads});
+  }
+}
---
+++ src/utils.js
@@ -0,0 +1,55 @@
+import httpStatus from 'http-status';
+import {createLogger} from '@natlibfi/melinda-backend-commons';
+import {Error as ApiError} from '@natlibfi/melinda-commons';
+import {logError} from '@natlibfi/melinda-rest-api-commons';
+
+export function checkStatus(response) {
+  const logger = createLogger();
+
+  // Unauthorized (401)
+  if (response.status === httpStatus.UNAUTHORIZED) { // eslint-disable-line functional/no-conditional-statement
+    logger.log('info', 'Got "UNAUTHORIZED" (401) response from record-load-api.');
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  // Forbidden (403)
+  if (response.status === httpStatus.FORBIDDEN) { // eslint-disable-line functional/no-conditional-statement
+    logger.log('info', 'Got "FORBIDDEN" (403) response from record-load-api.');
+    throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  // Not found (404)
+  if (response.status === httpStatus.NOT_FOUND) { // eslint-disable-line functional/no-conditional-statement
+    logger.log('info', 'Got "NOT_FOUND" (404) response from record-load-api. Process log files missing!');
+    throw new ApiError(httpStatus.NOT_FOUND, 'Process log not found!');
+  }
+
+  // Not acceptable (406)
+  if (response.status === httpStatus.NOT_ACCEPTABLE) { // eslint-disable-line functional/no-conditional-statement
+    logger.log('info', 'Got "NOT_ACCEPTABLE" (406) response from record-load-api. 0 processed records!');
+    throw new ApiError(httpStatus.NOT_ACCEPTABLE, '0 processed records!');
+  }
+
+  // Locked (423) too early
+  if (response.status === httpStatus.LOCKED) { // eslint-disable-line functional/no-conditional-statement
+    logger.log('info', 'Got "LOCKED" (423) response from record-load-api. Process is still going on!');
+    throw new ApiError(httpStatus.LOCKED, 'Not ready yet!');
+  }
+
+  // Service unavailable (503)
+  if (response.status === httpStatus.SERVICE_UNAVAILABLE) { // eslint-disable-line functional/no-conditional-statement
+    logger.log('info', 'Got "SERVICE_UNAVAILABLE" (503) response from record-load-api.');
+    throw new ApiError(httpStatus.SERVICE_UNAVAILABLE, 'The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later.');
+  }
+}
+
+export function handleConectionError(error) {
+  const logger = createLogger();
+  if (error.response) {
+    return error.response;
+  }
+
+  logger.log('info', 'No connection to aleph-record-load-api.');
+  logError(error);
+  throw new ApiError(httpStatus.SERVICE_UNAVAILABLE, 'The server is temporarily unable to service your request due to maintenance downtime or capacity problems. Please try again later.');
+}
